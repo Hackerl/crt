ea77faa951e2d5c8d44712577bbedbb50bb47045
Hackerl <490021209@qq.com>
2020-11-15 21:40:37 +0800

    Remove#macro semicolon

--- crt_asm.h
+++ crt_asm.h
@@ -2,12 +2,12 @@
 #define CRT_ASM_H
 
 #define CALL_SP(addr) \
-    asm volatile("mov %%rsp, %%rdi; call *%%rax;" : : "a"(addr));
+    asm volatile("mov %%rsp, %%rdi; call *%%rax;" : : "a"(addr))
 
 #define FIX_SP_JMP(stack, addr) \
-    asm volatile("xchg %%rsp, %0; jmp *%%rax;" : "=r"(stack) : "0"(stack), "a"(addr));
+    asm volatile("xchg %%rsp, %0; jmp *%%rax;" : "=r"(stack) : "0"(stack), "a"(addr))
 
 #define INJ_ENTRY(func) \
-    asm volatile("nop; nop; call " #func "; int3;");
+    asm volatile("nop; nop; call " #func "; int3;")
 
 #endif
