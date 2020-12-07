d3bb546e5b68719a9d3e0d8a22a9e616e42e4fe2
Hackerl <490021209@qq.com>
2020-12-07 12:44:10 +0800

    Add#read file utils

--- crt_utils.h
+++ crt_utils.h
@@ -2,7 +2,9 @@
 #define CRT_UTILS_H
 
 #include "crt_syscall.h"
+#include "crt_memory.h"
 #include <stdio.h>
+#include <fcntl.h>
 
 long get_file_size(int fd) {
     long fs, r;
@@ -30,6 +32,42 @@ long get_file_size(int fd) {
     _lseek(fd, 0, SEEK_SET);
 
     return fs;
+}
+
+int read_file(char *path, char **ptr) {
+    int fd = _open(path, O_RDONLY, 0);
+
+    if (fd < 0) {
+        return 0;
+    }
+
+    long file_size = get_file_size(fd);
+
+    if (file_size <= 0) {
+        _close(fd);
+        return 0;
+    }
+
+    char *buffer = malloc(file_size);
+
+    if (!buffer) {
+        _close(fd);
+        return 0;
+    }
+
+    memset(buffer, 0, file_size);
+
+    if (_read(fd, buffer, file_size) != file_size) {
+        free(buffer);
+        _close(fd);
+        return 0;
+    }
+
+    _close(fd);
+
+    *ptr = buffer;
+
+    return file_size;
 }
 
 #endif
